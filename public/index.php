7104767ca1ab81d7eb7c701c438aaf1522659c02
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-07 14:47:04 +0700

    Update index
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- public/index.php
+++ public/index.php
@@ -2,11 +2,14 @@
 <html>
 <head>
 	<title>Green Grass</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css"/>
 </head>
 <body>
 	<center>
-		<h1>Green Grass is under maintenance!</h1>
-		<h4>Ammar Faizi</h4>
+		<div>
+			<img id="logo" src="assets/img/green_grass_logo.png"/>
+		</div>
+		<marquee><h1>Welcome to Green Grass!</h1></marquee>
 	</center>
 </body>
 </html>
