baba3f6de87b16e0ad9b9d7f5a22a0c3cba5c56b
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-07 15:02:05 +0700

    Update index
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- public/index.php
+++ public/index.php
@@ -5,11 +5,33 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/index.css"/>
 </head>
 <body>
-	<center>
+	<div id="main-cage">
+		<marquee><h2>Green Grass is the solution!</h2></marquee>
 		<div>
 			<img id="logo" src="assets/img/green_grass_logo.png"/>
 		</div>
-		<marquee><h1>Welcome to Green Grass!</h1></marquee>
-	</center>
+		<h1>Welcome to Green Grass!</h1>
+		<div class="cdesc">
+			<h3>Key Partners</h3>
+			<p>We have farmers as key partners. Farmers have thecritical point for our business. Without them our business can't run.</p>
+		</div>
+		<div class="cdesc">
+			<h3>Revenue Streams</h3>
+			<p>Our revenue streams will be dynamic pricing, because every single item has its own price. So our revenue completely depends on how much an item is sold.</p>
+		</div>
+		<div class="cdesc">
+			<h3>Cost Structure</h3>
+			<p>Our business is cost driven, because we need to gather the resources at the beginning of steps. This takes a lot of money and to build trust between us and farmers, we need to assure that we have enough funding to continue distribute their vegetables, fruits, etc.</p>
+		</div>
+		<div id="table-cage">
+			<table>
+				<tr>
+					<td>Facebook</td>
+					<td>:</td>
+					<td>Green Grass</td>
+				</tr>
+			</table>
+		</div>
+	</div>
 </body>
 </html>
