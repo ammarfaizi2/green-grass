bb9eba88e3b2c9fe0ffde9354d3a14ba08e6d0d4
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-07 14:28:56 +0700

    Initial commit
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Green Grass</title>
+</head>
+<body>
+	<center>
+		<h1>Green Grass is under maintenance!</h1>
+		<h4>Ammar Faizi</h4>
+	</center>
+</body>
+</html>
