7c62f5d878cf0c3db8a33334e103fc7b490677fd
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-07 17:02:26 +0700

    Clean up
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- isolated/views/head.php
+++ isolated/views/head.php
@@ -1,3 +1,3 @@
 	<meta charset="utf-8"/>
 	<link rel="shortcut icon" type="image/png" href="<?= BASE_URL ?>/assets/img/green_grass_logo.png"/>
-	<link rel="stylesheet" type="text/css" href="assets/css/base.css?w=<?= cc_asset() ?>">
+	<link rel="stylesheet" type="text/css" href="assets/css/base.css?w=<?= cc_asset() ?>"/>
