fede2e2f20103e08676e21a9b8a57a359b560199
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-12 08:19:16 +0700

    Add tutorial 2
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ public/tips_dan_tutorial2.php
@@ -0,0 +1,82 @@
+<?php
+
+require __DIR__."/../init/web.php";
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Tips dan Tutorial dari Green Grass</title>
+<?php loadView("head") ?>
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css?w=<?= time() ?>"/>
+	<meta property="og:locale" content="en_US"/>
+	<meta property="og:url" content="<?= BASE_URL ?>"/>
+	<meta property="og:title" content="Green Grass"/>
+	<meta property="og:description" content="Tips dan Tutorial 2 dari Green Grass"/>
+	<meta property="og:image" content="<?= BASE_URL ?>/assets/img/sayur_asem.jpg"/>
+	<meta name="twitter:card" content="Tips dan Tutorial 2 dari Green Grass"/>
+	<meta name="twitter:site" content="@greengrass"/>
+	<meta name="twitter:title" content="Green Grass"/>
+	<meta name="twitter:description" content="Tips dan Tutorial 2 dari Green Grass"/>
+	<meta name="twitter:image" content="<?= BASE_URL ?>/assets/img/sayur_asem.jpg"/>
+</head>
+<body>
+	<div style="text-align:center;background-color:#fff;width:1000px;margin:auto;padding:20px;margin-top:80px;">
+		<h1>Tips Diet Sehat</h1>
+		<img src="assets/img/sayur2.jpg" alt="Sayur"/>
+		<h2>Memulai cara diet sehat</h2>
+		<div style="text-align:left;">
+			<p>Diet sehat pertama yang perlu Anda lakukan adalah dengan olahraga secara rutin. Olahraga yang dimaksud bukanlah naik tangga kantor atau memilih berjalan kaki menuju kantor, ya. Olahraga untuk menurunkan berat badan benar-benar perlu terprogram dengan baik.</p>
+
+			<h2>1. Olahraga</h2>
+			<p>Diet sehat pertama yang perlu Anda lakukan adalah dengan olahraga secara rutin. Olahraga yang dimaksud bukanlah naik tangga kantor atau memilih berjalan kaki menuju kantor, ya. Olahraga untuk menurunkan berat badan benar-benar perlu terprogram dengan baik. Mengutip dari Diet Doctor (dietdoctor.com), Anda perlu olahraga minimal 30 hingga 60 menit per hari. Namun, pastikan untuk tidak mengonsumsi sport drink, ya. Jenis minuman itu dapat membuat proses menurunkan berat badan menjadi kurang efektif.</p>
+
+			<h2>2. Hindari Stres</h2>
+			<p>Cara diet sehat selanjutnya yang sebaiknya Anda coba adalah dengan menghindari stres berlebih dan kurang tidur. Dua hal itu dapat meningkatkan hormon stres dalam tubuh sehingga rasa lapar pun meningkat. Artinya, keinginan untuk mengonsumsi makanan tidak dapat Anda hindari. Cobalah untuk mengelola stres dengan baik. Misalnya, lakukan hobi atau kegiatan yang bisa membuat tubuh dan pikiran rileks, menulis jurnal, dan lain sebagainya. Mengatur waktu tidur menjadi lebih awal pun bisa Anda lakukan.</p>
+
+			<h2>3. Kurangi Konsumsi Produk Olahan Susu</h2>
+			<p>Produk olahan susu atau dairy product memang terasa lezat. Sayangnya, jenis makanan itu tidak cocok dimasukkan dalam menu diet sehat. Jenis makanan itu mengandung banyak laktosa sehingga proses penurunan berat badan menjadi lebih lambat.</p>
+
+			<h2>4. Jangan Terlalu Banyak Makan Kacang</h2>
+			<p>Kacang adalah salah satu jenis makanan yang mengandung banyak karbohidrat, apalagi kacang mete. Jenis kacang itu mengandung karbohidrat sebanyak 20% sehingga tidak cocok dikonsumsi ketika diet. Jadi, cobalah untuk lebih berhati-hati ketika makan kacang supaya berat badan bisa turun secara efektif.</p>
+
+			<h2>5. Jangan Minum Bir</h2>
+			<p>Bir adalah salah satu jenis minuman beralkohol yang sebaiknya tidak Anda konsumsi secara berlebih. Sama seperti poin sebelumnya, konsumsi bir dapat menurunkan proses pembakaran lemak sehingga berat badan sulit turun. Jika Anda benar-benar ingin mengonsumsi minuman beralkohol, cobalah untuk mengonsumsi jenis minuman yang lain. Mulai dari wine, dry champagne, atau hard liquor. Meski demikian, Anda tetap perlu mengatur porsi minuman yang Anda konsumsi.</p>
+		</div>
+
+		<h1>Tutorial Membuat Sayur Asem</h1>
+		<img src="assets/img/sayur_asem.jpg" alt="Sayur Asem"/>
+		<div style="text-align:left;">
+			<p>Jika sayur asem yang Anda masak biasanya tidak pakai sayuran kangkung, kali ini Anda patut mencobanya. Kangkung bukan hanya bisa dimasak tumis atau dibuat cah, tetapi juga dapat menjadi bahan utama dalam sayur asem. Kangkung memiliki banyak manfaat untuk kesehatan. Di antaranya menjaga kesehatan mata hingga mencegah dehidrasi.</p>
+			<h2>Bahan:</h2>
+			<ul>
+				<li>3 ikat kangkung, bersihkan dan potong</li>
+				<li>100 gram kacang merah segar</li>
+				<li>4 buah tomat, potong jadi empat bagian</li>
+				<li>700 cc air</li>
+			</ul>
+			<h2>Bumbu:</h2>
+			<ul>
+				<li>2 siung bawang putih</li>
+				<li>7 siung bawang merah</li>
+				<li>3 buah cabai merah besar</li>
+				<li>2 butir kemiri</li>
+				<li>1/2 sdt terasi matang</li>
+				<li>Sepotong lengkuas, memarkan</li>
+				<li>3 lembar daun salam</li>
+				<li>Air asam secukupnya sesuai selera</li>
+				<li>Garam secukupnya</li>
+				<li>Gula secukupnya</li>
+			</ul>
+			<h2>Cara Pembuatan:</h2>
+			<ol>
+				<li>Rebus kacang merah sampai lunak.</li>
+				<li>Haluskan bawang putih, bawang merah, cabai merah, kemiri, dan terasi matang.</li>
+				<li>Didihkan air di panci dan masukkan semua bumbu yang telah dihaluskan.</li>
+				<li>Tambahkan daun salam, lengkuas, air asam, garam, dan gula.</li>
+				<li>Masukkan kangkung, tomat, dan kacang merah secara bersamaan. Masak hingga matang.</li>
+			</ol>
+			<p>Demikian cara membuat sayur asem sapi istimewa ala rumahan. Tak kalah enak dengan bikinan resto, kok.</p>
+		</div>
+	</div>
+</body>
+</html>
